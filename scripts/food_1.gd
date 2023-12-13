dcc918486db8a6b2e80af2fd5efb1a30e9bac640
j-hnn <john.weng@searcyschools.org>
2023-12-13 14:15:24 -0600

    removed extra unneeded code

--- scripts/food_1.gd
+++ scripts/food_1.gd
@@ -6,15 +6,13 @@ signal inside
 
 func _physics_process(delta):
 	global_position.y += speed * delta
-	
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	die()
 
 func die():
 	queue_free()
 
 func _on_area_entered(area):
 	if area is base:
+		die()
 		inside.emit(true)
 
 func _on_area_exited(area):
