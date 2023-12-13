b740cae354d59a8bb61dc20575bf6116435399d6
j-hnn <john.weng@searcyschools.org>
2023-12-13 14:10:35 -0600

    added completed burgers

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @export var food_scenes: Array[PackedScene] = []
+@export var burger_scene: Array[PackedScene] = []
 
 @onready var food_spawn_location = $FoodSpawnLocation
 @onready var food_container = $FoodContainer
+@onready var completed_burgers = $CompletedBurgers
 @onready var stack_position = $stack_position
 @onready var countdown_timer = $CountdownTimer
 @onready var time_left = $timeLeft
@@ -14,8 +16,8 @@ extends Node2D
 var food_number = 0
 var total_burgers = 0
 var speed_multiplier = 1
+var complete_burger_x = 0
 var inornot = false
-
 var start_time = 300
 var sec = start_time
 
@@ -24,6 +26,8 @@ func game_time():
 		sec -= 1
 		time_left.text = str(sec)
 		time_progress_bar.value -= 1
+	if total_burgers == 5:
+		get_tree().change_scene_to_file("res://win_screen.tscn")
 
 func _ready():
 	time_progress_bar.value = start_time
@@ -47,7 +51,6 @@ func spawn_food():
 	food.inside.connect(_inside_bun)
 	food_container.add_child(food)
 	
-	
 func _inside_bun(torf):
 	if torf == true:
 		inornot = true
@@ -65,8 +68,15 @@ func update_food():
 			total_burgers += 1
 			speed_multiplier += 0.75
 			travis.play("celebrate")
-	elif total_burgers == 5:
-		get_tree().change_scene_to_file("res://win_screen.tscn")
+			add_burger_complete()
+			await get_tree().create_timer(1.0).timeout
+			travis.play("idle")
 
 func _on_countdown_timer_timeout():
 	game_time()
+
+func add_burger_complete():
+	var burger = burger_scene[0].instantiate()
+	burger.global_position = Vector2(complete_burger_x, 559)
+	completed_burgers.add_child(burger)
+	complete_burger_x += 100
